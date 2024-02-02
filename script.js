6f35d4512daec96f07b4bdc60e00bd325fcae6ca
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2024-02-02 21:43:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	let width,height;
+	constructor(width,height);
+	function getArea(){
+     return width*height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	getPerimeter(){
+      2*width;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
